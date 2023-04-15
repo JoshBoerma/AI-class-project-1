e5141b78cb3fbd84364ca8ed08f34fb20d1f5c7b
Joshua Boerma <jboerma@mines.edu>
2023-04-14 20:29:57 -0600

    Reading command arguments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main(int argc, char* argv[]){
-    std::cout << "Hello World!" << std::endl;
+    if(argc != 4){
+        cout << "Please input the correct amount of arguments" << endl;
+        return -1;
+    }
+    string file = argv[1];
+    string city1 = argv[2];
+    string city2 = argv[3];
+
+    return 0;
 }
 
